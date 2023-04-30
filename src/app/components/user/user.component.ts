37381817adca2abc917ebd97d742528b04fd1884
Nodirbek-Abdulaxadov <nbkabdulaxadov@gmail.com>
2023-04-30 23:13:36 +0500

    next beta version

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/models/User';
@@ -11,12 +12,35 @@ import { UserService } from 'src/app/services/user/user.service';
 })
 export class UserComponent implements OnInit {
 
-  constructor(private userService: UserService){}
+  constructor(private userService: UserService,
+              private formBuilder: FormBuilder){}
 
   count: number = 0;
   users: Observable<User[]> | undefined;
 
   ngOnInit(): void {
     this.users = this.userService.getAll('admins');
+  }
+
+  fullName = new FormControl('',[
+    Validators.required,
+    Validators.minLength(5),
+    Validators.maxLength(100)
+  ])
+
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(9),
+    Validators.maxLength(15)
+  ])
+
+  public addnewForm = this.formBuilder.group({
+    fullName: this.fullName,
+    phoneNumber: this.phoneNumber
+  });
+
+
+  addNew() {
+    console.log(this.addnewForm.getRawValue())
   }
 }
